5e1d256e3d81173089f0174ce83846aa0295deb3
Mayank Aitan <137062191+mayank013p@users.noreply.github.com>
2024-04-19 21:38:47 +0530

    Update 0121-best-time-to-buy-and-sell-stock.java

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,7 +1,5 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        // Start typing your Java solution below
-        // DO NOT write main() function
         if(prices == null || prices.length == 0) return 0;
         int min = prices[0];
         int max = Integer.MIN_VALUE;
